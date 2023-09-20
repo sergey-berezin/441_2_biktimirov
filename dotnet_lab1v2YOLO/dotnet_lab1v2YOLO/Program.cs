e7aa01b4db1db8ae7823df91f9c31e9b2bce25e0
Bikmish <bikmish@ispras.ru>
2023-09-20 13:49:28 +0300

    Initial commit: YOLOlib - solution with source of YOLO-component, dotnet_lab - solution with two console programs representing sync (using threads) and async (using tasks) object detection

---
+++ dotnet_lab1v2YOLO/dotnet_lab1v2YOLO/Program.cs
@@ -0,0 +1,57 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Net;
+
+namespace dotnet_lab1v2YOLO
+{
+    internal class Program
+    {
+        public class FileServices : YOLOlib.IFileServices
+        {
+            public void Download(string url, string fileName)
+            {
+                using (var client = new WebClient())
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            client.DownloadFile(url, fileName);
+                        }
+                        catch (WebException)
+                        {
+                            continue;
+                        }
+                        break;
+                    }
+                }
+            }
+            public bool Exists(string path) => File.Exists(path);
+        }
+        static void Main(string[] args)
+        {
+            var detector = new YOLOlib.Detector(new FileServices());
+            var threads = Enumerable.Range(0, args.Length).Select(i =>
+            {
+                var thread = new Thread(o =>
+                {
+                    lock(detector)
+                        SaveResults(detector.Analyze(args[(int)o]));
+                });
+                thread.Start(i);
+                return thread;
+            }).ToArray();
+
+            foreach(var thread in threads)
+                thread.Join();
+        }
+
+        private static void SaveResults((string fileName, string curCsv, Image<Rgb24> curImg) res)
+        {
+            if (!File.Exists("results.csv") || !File.ReadLines("results.csv").Any(s => String.CompareOrdinal(s + "\n", res.curCsv) == 0))
+                File.AppendAllText("results.csv", res.curCsv);
+
+            res.curImg.Save(res.fileName);
+        }
+    }
+}
---
+++ dotnet_lab1v2YOLO/dotnet_lab1v2YOLOasync/Program.cs
@@ -0,0 +1,67 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Net;
+
+namespace dotnet_lab1v2YOLO
+{
+    internal class Program
+    {
+        public class FileServices : YOLOlib.IFileServices
+        {
+            public void Download(string url, string fileName)
+            {
+                using (var client = new WebClient())
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Downloading...");
+                            client.DownloadFile(url, fileName);
+                            Console.WriteLine("Succesfully downloaded!");
+                        }
+                        catch (WebException)
+                        {
+                            continue;
+                        }
+                        break;
+                    }
+                }
+            }
+            public bool Exists(string path) => File.Exists(path);
+        }
+        static async Task Main(string[] args)
+        {
+            var detector = new YOLOlib.Detector(new FileServices());
+            var tokenSource = new CancellationTokenSource();
+            var tasks = Enumerable.Range(0, args.Length).Select(i =>
+            {
+                return detector.Analyze(args[(int)i], tokenSource.Token);
+            });
+
+            Console.WriteLine("Press Enter to continue or type 'c' to cancel detection");
+            if (Console.ReadLine() == "c")
+                tokenSource.Cancel();
+
+            try
+            {
+                await Task.WhenAll(tasks);
+                foreach (var res in tasks)
+                    SaveResults(res.Result);
+            }
+            catch (Exception ex) when (ex is OperationCanceledException || ex is TaskCanceledException)
+            {
+                Console.WriteLine("Detecting cancelled!");
+            }
+
+        }
+
+        private static void SaveResults((string fileName, string curCsv, Image<Rgb24> curImg) res)
+        {
+            if (!File.Exists("resultsAsync.csv") || !File.ReadLines("resultsAsync.csv").Any(s => String.CompareOrdinal(s + "\n", res.curCsv) == 0))
+                File.AppendAllText("resultsAsync.csv", res.curCsv);
+
+            res.curImg.Save(res.fileName);
+        }
+    }
+}
