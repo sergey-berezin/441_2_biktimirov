c7275831891891c25650646b789bb3d12c57b03b
Bikmish <bikmish@ispras.ru>
2023-10-11 12:51:58 +0300

    Creating user interface, refactoring to MVVM

---
+++ dotnet_lab1v2YOLO/YoloView/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Forms;
+using YoloViewModel;
+
+namespace YoloView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel(new FileService(), new DirectoryService());
+        }
+    }
+
+    public class FileService : YOLOlib.IFileServices
+    {
+        public void Download(string url, string fileName)
+        {
+            using (var client = new WebClient())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        client.DownloadFile(url, fileName);
+                    }
+                    catch (WebException)
+                    {
+                        continue;
+                    }
+                    break;
+                }
+            }
+        }
+        public bool Exists(string path) => File.Exists(path);
+
+        public void Print(string msg) => Console.WriteLine(msg);
+    }
+
+    public class DirectoryService : IDirectoryServices
+    {
+        public List<string> ChooseFolder()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Choose folder with source images.";
+            var fileList = new List<string>();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var file in Directory.GetFiles(fbd.SelectedPath))
+                    fileList.Add(file);
+            }
+            return fileList;
+        }
+
+        public void Print(string msg) => System.Windows.MessageBox.Show(msg);
+    }
+}
---
+++ dotnet_lab1v2YOLO/YoloViewModel/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace YoloViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] String propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
